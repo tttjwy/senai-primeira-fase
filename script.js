d19e7dc9890df486b4414d8a5b3b4deea747e34b
tttjwy <gustavomachadobr909@gmail.com>
2025-09-04 21:52:26 -0300

    algumas atualizações nos primeiros projetos de HTML

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("Olá mundo!")
+// alert("Aula de javascript")
+// alert("Bem vindo ao  javascript")
+// alert("Tudo bem?")
+// alert("Adeus!")
+
+// let idade 
+// idade = 18
+// alert(idade)
+// idade = "Melões"
+// alert(idade)
+
+const amor = "nada de amor"
+alert(amor)
+
+amor = "nada de amor pt2"
+alert(amor)
+
+var antigao = "não devo usar essa porra"
