34af9d7a370ff4f25c11fdb1b53aedc4cf11b57a
tttjwy <gustavomachadobr909@gmail.com>
2025-09-09 22:11:47 -0300

    logica com js

---
+++ logica-js/variaveis.js
@@ -0,0 +1,26 @@
+// variavel com var
+// var nome = "Gustavo"
+// var nome = "silvio"
+// nome = "Silvio"
+
+// variavel com let
+// let nome = "Gustavo"
+// nome = "Silvio"
+
+// variavel declarada com const
+const nome = "Gustavo" //string
+const sobrenome = "Machado" 
+const idade = 18 //number
+const masculino = true //boolean
+
+//arrays
+const frutas = ["maçã", "banana", "laranja"]
+
+console.log(frutas[0])
+
+
+
+
+
+// console.log("Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos.")
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos.`) //template string
