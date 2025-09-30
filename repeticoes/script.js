1ea348f3387fc95a0d6ca0d13d33eec4271668a8
tttjwy <gustavomachadobr909@gmail.com>
2025-09-29 22:15:56 -0300

    repeticoes

---
+++ repeticoes/script.js
@@ -0,0 +1,12 @@
+//let acertos = Number(prompt("Quantos acertos?"))
+//let valorPorQuestao = 10/16
+//let nota = acertos * valorPorQuestao
+//alert(`Sua nota é ${nota.toFixed(2)}`)
+
+let contador = 0
+
+while(contador < 2){
+    contador = contador + 1
+    console.log(`Contador: ${contador}`)
+
+}   
