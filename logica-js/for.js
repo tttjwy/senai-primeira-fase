eadc5d67a14ee676cbc2c72e4bf02c939f5789ab
tttjwy <gustavomachadobr909@gmail.com>
2025-09-30 22:09:46 -0300

    funcoes

---
+++ logica-js/for.js
@@ -0,0 +1,28 @@
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i)
+// }
+
+// for (let i = 0; i <= 20; i += 5) {
+//     console.log(i)
+// }
+
+// let soma = 0
+// for (let i = 1; i < 100; i++) {
+//     soma += i
+// }
+// console.log("resultado da soma ", soma)
+
+// let alunos = ["Gustavo", "Juan", "Alejandro"]
+// alunos.push("Maria")
+// for (let i = 0; i < alunos.length; i+=) {
+//     console.log("Aluno: ", alunos[i])
+// }
+
+let frutas = ["Banana", "Maçã", "Pera", "Uva", "Abacaxi", "Laranja"]
+for (let i = 0; i < frutas.length; i+=3) {
+    console.log("Fruta: ", frutas[i])
+}
---
+++ logica-js/funcoes.js
@@ -0,0 +1,27 @@
+// function saudacao(nome){
+//     console.log(`Bom dia, ${nome}!`)
+// }
+
+// const saudacao = (nome) => {
+//     console.log(`Bom dia, ${nome}!`)
+// }
+
+// saudacao("Gustavo")
+// saudacao("Juan")
+
+const soma = (x,y) => x+y //arrow function com return implicito
+console.log(`Resultado da soma: ${soma(10,5)}`)
+// const soma = (x,y) => {
+//     return x+y
+//  }
+
+const subtracao = (x,y) => x-y 
+console.log(`Resultado da subtração: ${subtracao(10,5)}`)
+
+const multiplicacao = (x,y) => x*y
+console.log(`Resultado da multiplicação: ${multiplicacao(10,5)}`)
+
+const divisao = (x,y) => x/y
+console.log(`Resultado da divisão: ${divisao(10,5)}`)
+
+
