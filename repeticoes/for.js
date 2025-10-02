9736771dd7a1054ad2196bdd4c4acf9db61a0fd6
tttjwy <gustavomachadobr909@gmail.com>
2025-10-02 20:34:40 -0300

    repetiçoes com for

---
+++ repeticoes/for.js
@@ -0,0 +1,21 @@
+// let i = 0
+// while (i < 3) {
+//     alert("Valor de i: " + i)
+//     i++
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     alert("Valor de i: " + i)
+// }
+
+// let soma = 0
+// for(let i=0; i<10; i++){
+//     let nota = Number(prompt(`Digite ${i+1}a nota: `))
+//     soma += nota
+// }
+// alert(`A soma das notas é: ${soma}`)
+
+const nomes = ["Dona Bete","Mano Juca","Lucio Fernando", "Capitao Ganso"] 
+for(let i=0; i<nomes.length; i++){
+    alert(nomes[i])
+}
