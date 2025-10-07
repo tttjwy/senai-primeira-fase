d4db5dc9a07f802da33e65c9bf08519d746316a9
tttjwy <gustavomachadobr909@gmail.com>
2025-10-06 21:42:18 -0300

    mais exercicios de repetiçao

--- repeticoes/for-exercicios.js
+++ repeticoes/for-exercicios.js
@@ -1,4 +1,3 @@
-// let quadrado = 0
 // for (let i = 1; i <= 10; i++) {
 //     quadrado = i * i
 //     alert(`O quadrado de ${i} é ${quadrado}`)
@@ -8,26 +7,48 @@
 //     alert(i)
 // }
 
-// let soma = 0
 // for (let i = 1; i < 100; i++) {
 //     soma += i
 //     alert(`A soma é ${soma}`)
 // }
 
-// let tabuada = 0
 // for (let i = 1; i <= 10; i++) {
 //     tabuada = 5 * i
 //     alert(`5 x ${i} = ${tabuada}`)
-// }
+//  }
 
-// let tabuada = 0
 // let numero = Number(prompt("Digite a tabuada que deseja: "))
 // for (let i = 1; i <= 10; i++) {
 //    tabuada = numero * i
 //     alert(`${numero} x ${i} = ${tabuada}`)
 //  }
 
-let numero = 0
-for (let i = 1; i <= 20; i++) {
-    numero = i % 2 == 0
-}
+// for (let i = 1; i <= 20; i++) {
+//     numero = i % 2 == 0
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     alert(i)
+// }
+// for (let i = 10; i >= 0; i--) {
+//     alert(i)
+// }
+
+
+// Parte 2
+
+// let palavra = prompt("Digite uma palavra: ")
+// for (let i = 0; i <= 10; i++) {
+//     alert(palavra)
+// }
+
+// let numero = Number(prompt("Digite um número: "))
+// for (let i = 0; i <= numero; i+=2) {
+//     alert(i)
+// }
+
+// let numero = Number(prompt("Digite um número: "))
+// for (let i = numero; i >= 0; i -= 2) {
+//     numero = i % 2 == 3
+//     alert(i)
+// }
