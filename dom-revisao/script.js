cb7c52340ea391a1d07c1999cd17c1fc36bde0f1
tttjwy <gustavomachadobr909@gmail.com>
2025-10-23 21:56:38 -0300

    exercicios dom

---
+++ dom-revisao/script.js
@@ -0,0 +1,35 @@
+// let usuario
+
+// function cadastrarUsuario(){
+//     console.log("Cadastrou o usuário")
+//     usuario = document.getElementById("inputUsuario").value
+//     document.getElementById("inputUsuario").value = ""
+//     document.getElementById("resultado").innerHTML = "Usuário " + usuario + " cadastrado com sucesso"
+// }
+    
+// function consumoCombustivel(){
+//     let distancia = document.getElementById("inputDistancia").value
+//     let litros = document.getElementById("inputCombustivel").value
+//     let consumo = distancia / litros
+//     document.getElementById("resultado").innerHTML = "O consumo médio é de " + consumo.toFixed(2) + " km/l"
+// }
+
+// function salarioTotal(){
+//     let salario = document.getElementById("inputSalarioHora").value
+//     let horas = document.getElementById("inputHoras").value
+//     let valorHora = salario * horas
+//     document.getElementById("resultado").innerHTML = "O valor do salário total é de R$ " + valorHora.toFixed(2)
+// }
+
+// function MediaPonderada(){
+//     let nota1 = parseFloat(document.getElementById("inputNota1").value)
+//     let nota2 = parseFloat(document.getElementById("inputNota2").value)
+//     let nota3 = parseFloat(document.getElementById("inputNota3").value)
+//    document.getElementById("resultado").innerHTML = "A média ponderada é de " + (nota1*2 + nota2*3 + nota3*5) / (2 + 3 + 5)     
+// }
+
+function MediaPonderada(){
+    let nota1 = parseFloat(document.getElementById("inputNota1").value)
+    let nota2 = parseFloat(document.getElementById("inputNota2").value)
+   document.getElementById("resultado").innerHTML = "A média ponderada é de " + (nota1*3.5 + nota2*7.5) / (3.5 + 7.5)     
+}
