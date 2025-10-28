9360494ad1588e2a74563ed367fcccaa3c44706f
tttjwy <gustavomachadobr909@gmail.com>
2025-10-27 22:01:35 -0300

    exercicio 25

--- dom-revisao/script.js
+++ dom-revisao/script.js
@@ -30,6 +30,21 @@
 
 // function MediaPonderada(){
 //     let nota1 = parseFloat(document.getElementById("inputNota1").value)
-//     let nota2 = parseFloat(document.getElementById("inputNota2").value)
-//    document.getElementById("resultado").innerHTML = "A média ponderada é de " + (nota1*3.5 + nota2*7.5) / (3.5 + 7.5)     
+//    let nota2 = parseFloat(document.getElementById("inputNota2").value)
+//   document.getElementById("resultado").innerHTML = "A média ponderada é de " + (nota1*3.5 + nota2*7.5) / (3.5 + 7.5)     
 // }
+
+function Porco(){
+    let produtoX = document.getElementById("inputProdutoX").value
+    let moeda1 = document.getElementById("inputMoedas1").value
+    let moeda050 = document.getElementById("inputMoedas050").value
+    let moeda025 = document.getElementById("inputMoedas025").value
+    let moeda010 = document.getElementById("inputMoedas010").value
+    let moeda005 = document.getElementById("inputMoedas005").value
+    let total = (moeda1 * 1) + (moeda050 * 0.50) + (moeda025 * 0.25) + (moeda010 * 0.10) + (moeda005 * 0.05)   
+    if(total >= produtoX){
+        document.getElementById("resultado").innerHTML = `O produto ${produtoX}R$ cabe dentro do orçamento de ${total.toFixed(2)} moedas`
+    } else {
+        document.getElementById("resultado").innerHTML = `O produto ${produtoX}R$ não cabe dentro do orçamento de ${total.toFixed(2)} moedas`
+    }
+}
